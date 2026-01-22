880cb5606f27bc11415f6bc24348c54df78a9d42
EliasGuedra <90322377+EliasGuedra@users.noreply.github.com>
2026-01-22 14:19:01 +0100

    spelled porly...

--- lecture_a/sequencer.py
+++ lecture_a/sequencer.py
@@ -1,11 +1,13 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.parse_args()
+
+parser.add_argument("--sequence",
+                    choices=["fibonacci", "square", "triangular", "factorial"],
+                    required=True)
 
 
-
-def fibinacci(n):
+def fibonacci(n):
 
     l = [0, 1]
 
@@ -15,8 +17,22 @@ def fibinacci(n):
     return l [1:]
 
 
-def prime_numbers(n):
+def square(n):
+    return [i**2 for i in range(1,n+1)]
 
-    l = []
 
+def triangular(n):
+
+    l = [0]
+
+    for i in range(1, n+1):
+        l.append(l[-1] + i)
+    return l [1:]
+
+def factorial(n):
+    l = [1]
+
+    for i in range(1, n+1):
+        l.append(l[-1]*i)
+    return l [1:]
 
